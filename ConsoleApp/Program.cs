0fe29f19676a930463c4162e45fcb8de4b8ed76b
Brad Kilkenney <bkilkenney@gmail.com>
2016-10-25 12:56:45 -0400

    Initial Commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fruitList = new List<string>();
+            //B/c it's new I'm calling the list classes constructor method with () at the end
+            fruitList.Add("Orange");
+            fruitList.Add("Strawberry");
+            fruitList.Add("Banana");
+            fruitList.Add("Fig");
+
+            foreach (var item in fruitList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+
+            fruitList.Sort();                   //Sorts by asc, alphabetical when used on strings
+            foreach (var item in fruitList)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+
+            ReportOnApples(fruitList);
+
+            Console.ReadLine();
+        }
+
+        static void ReportOnApples(List<string> items)
+        {
+            string AppleReport = items.Contains("Apple", StringComparer.OrdinalIgnoreCase) ?
+                "Yes there are apples." :
+                "No there are no apples.";
+
+            Console.WriteLine(AppleReport);
+        }
+
+    }
+}
